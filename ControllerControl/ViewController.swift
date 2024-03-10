f2a4d4d5ce12cb6573d20efee08ed77f471766b6
Acane <listencpp@gmail.com>
2024-03-10 10:40:49 +0800

    Initial Commit

---
+++ ControllerControl/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ControllerControl
+//
+//  Created by Acane on 2024/3/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
